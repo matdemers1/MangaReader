990ca85e019630d0d07d34d637e787dbcebf3c76
Mat Demers <matthew.demers@codingcape.com>
2024-01-21 00:32:08 -0500

    Create some mock data

---
+++ MangaReader/MockData/MockMangaObject.swift
@@ -0,0 +1,8 @@
+//
+//  MockMangaObject.swift
+//  MangaReader
+//
+//  Created by Matthew Demers on 1/20/24.
+//
+
+import Foundation
---
+++ MangaReader/MockData/MockTags.swift
@@ -0,0 +1,8 @@
+//
+//  MockTags.swift
+//  MangaReader
+//
+//  Created by Matthew Demers on 1/21/24.
+//
+
+import Foundation
